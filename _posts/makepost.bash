d3c6c3ee9be16def962ee036c7c6226a8d41da80
lesguillemets <lesguillemets@users.noreply.github.com>
2014-10-07 01:06:42 +0900

    makepost.bash : add option for gvim

--- _posts/makepost.bash
+++ _posts/makepost.bash
@@ -1,4 +1,8 @@
 filename="`date "+%Y-%m-%d-"`$1".md
 cp -i ./template.md "$filename"
 echo "Created $filename"
-vim $filename
+if [ "$2" == "-g" ] || [ "$2" == "--gvim" ] || [ "$2" == "--gui" ]; then
+    gvim $filename
+else
+    vim $filename
+fi
